bc97e6c91b201773a5acaa00e89e1e94a87da9e1
Praveen M <69027747+PraveenM24@users.noreply.github.com>
2020-08-01 12:24:00 +0530

    Update DICE_def.py

--- DICE_def.py
+++ DICE_def.py
@@ -1,9 +1,12 @@
+#This contains the functions for the Main game
 s= "+- - - -+"
 m1="| o   o |"
 m2="| o     |"
 m3="|   o   |"
 m4="|     o |"
 m5="|       |"
+
+#This function is used to determine the dice phase based on the random value
 def dice(choice):
     if choice == 1:
         print(s + "\n" + m5 + "\n" + m3 + "\n" + m5 + "\n" + s)
@@ -18,6 +21,7 @@ def dice(choice):
     elif choice == 6:
         print(s + "\n" + m1 + "\n" + m1 + "\n" + m1 + "\n" + s)
 
+#This function is used to check whether the user's account balance is sufficient to get the Bounty tries        
 def validate(d_amount,count):
     c_amount=50*count
     if d_amount<c_amount:
